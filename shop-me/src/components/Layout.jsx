41be3e9b38ca8cca69a213fd37d3abe9b05e6f9d
nazvath <nasvaabbas@gmail.com>
2024-08-12 19:19:49 +0400

    first commit

---
+++ shop-me/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+function Layout() {
+  return (
+    <>
+    <div>
+<Header/>
+</div>
+<Outlet/>
+</>
+  )
+}
+
+export default Layout
